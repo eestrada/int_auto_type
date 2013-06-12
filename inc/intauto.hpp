940a3b0c9a42edbae4ff2966028701c94a046719
Ethan Estrada <ethan.estrada@gmail.com>
2013-06-12 00:41:54 -0600

    Declare more member functions

--- inc/intauto.hpp
+++ inc/intauto.hpp
@@ -9,9 +9,30 @@ namespace iat
 class intauto_t
 {
 public:
-    intauto_t() throw(std::runtime_error);
+    intauto_t() throw(std::bad_alloc);
+
+    intauto_t(const intauto_t &other) throw(std::runtime_error);
+
+    template < typename N >
+    intauto_t(const N &other) throw(std::runtime_error);
+
     ~intauto_t() noexcept;
+
+    intauto_t & operator=(const intauto_t &other);
+
+    template < typename N >
+    intauto_t & operator=(const N &other);
+
+    intauto_t operator+(const intauto_t &other);
+
+    intauto_t operator-(const intauto_t &other);
+
+    intauto_t operator*(const intauto_t &other);
+
+    intauto_t operator/(const intauto_t &other);
 private:
+
+    char *data;
 };
 
 } //End iat namespace
--- src/intauto.cpp
+++ src/intauto.cpp
@@ -3,12 +3,28 @@
 namespace iat
 {
 
-intauto_t::intauto_t() throw (std::runtime_error)
+intauto_t::intauto_t() throw(std::bad_alloc)
+{
+    data = new char[2];
+
+    data[0] = '0'; data[1] = '\0';
+}
+
+intauto_t::intauto_t(const intauto_t &other) throw(std::runtime_error)
 {
     throw std::runtime_error("intauto_t not implemented");
 }
 
-intauto_t::~intauto_t() noexcept {}
+template < typename N >
+intauto_t::intauto_t(const N &other) throw(std::runtime_error)
+{
+    throw std::runtime_error("intauto_t not implemented");
+}
+
+intauto_t::~intauto_t() noexcept
+{
+    delete[] data;
+}
 
 } //End iat namespace
 
